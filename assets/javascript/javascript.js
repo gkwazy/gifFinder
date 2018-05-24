e47500522a75fa257d02caaa1e8c7b1ffaeb9946
gkwazy <noreply>
2018-05-24 12:54:19 -0600

    gif show not to add buttons

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,6 +15,28 @@ function buttonMaker() {
     }
 }
 
-function gifSearch() {
+function buttonSelected() {
 
 }
+
+$("#add-gif").on("click", function (event) {
+    event.preventDefault();
+    var wantedGif = $("#searchInput").val();
+    var quaryURL = "http://api.giphy.com/v1/gifs/search?q=" + wantedGif + "&api_key=krsfidvpUvoa9TjrFjFKcRZif8FtU45h&limit=200"
+
+    $.ajax({
+        url: quaryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        for (var i = 0; i < 10; i++) {
+            randomGif = Math.floor(Math.random() * 200);
+            console.log(randomGif);
+            ($("#gifs").prepend("<img src=" + (response.data[randomGif].images.original.url) + " height = 400 width = 400>\n"));
+            ($("#gifs").prepend("<p>" + (response.data[randomGif].rating) + "</p>\n"));
+
+
+
+        }
+    })
+});
